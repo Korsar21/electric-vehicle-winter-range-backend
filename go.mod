0876c7211ee5e3024b5d99089128df25d18d519a
Korsar21 <kosart2006@gmail.com>
2026-09-02 14:37:56 +0300

    Initialize Go project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Korsar21/electric-vehicle-winter-range-backend
+
+go 1.26.5
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Electric Vehicle Winter Range")
+}
